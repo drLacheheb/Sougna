3de0a9675fe42b4bb48c8c2eb3bdf34667444d48
AbderrazzakAiba <aibaabderrazzak7@gmail.com>
2025-02-19 08:43:34 +0100

    تعديلات جديدة

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
     // ViewModel Kotlin extensions for easier ViewModel usage
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("io.coil-kt:coil-compose:2.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
